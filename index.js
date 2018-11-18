63975a615e28979727c60e24264bdc137d102d8d
Andrew Jivoin <andrew@jivoin.com>
2018-11-18 13:51:35 -0500

    Adds basic functions

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ document.getElementById('importConversation').onclick = function() {
   }
 
   fr.readAsText(files.item(0));
+  document.getElementById('importConversation').style = 'display: none';
+  document.getElementById('selectConversation').style = 'display: none';
+  document.getElementById('conHeader').style = 'display: none';
 };
 
 document.getElementById('importMessage').onclick = function() {
@@ -33,9 +36,59 @@ document.getElementById('importMessage').onclick = function() {
   }
 
   fr.readAsText(files.item(0));
+  document.getElementById('importMessage').style = 'display: none';
+  document.getElementById('selectMessage').style = 'display: none';
+  document.getElementById('mesHeader').style = 'display: none';
 };
 
-function mostMessages() {
+/**
+ * Returns a dictionary mapping user_id to a list of their messages
+ */
+function mapUserToMessages() {
   members = conversation.members;
+  result = {}
   
+  messages.forEach(m => {
+    if (result.hasOwnProperty(m.user_id)) {
+      result[m.user_id].push(m);
+    } else {
+      result[m.user_id] = [m]
+    }
+  });
+
+  return result;
+}
+
+/**
+ * Returns dictionary mapping user_id to number of messages sent
+ */
+function countMessages() {
+  members = conversation.members;
+  result = {}
+  
+  messages.forEach(m => {
+    if (result.hasOwnProperty(m.user_id)) {
+      result[m.user_id]++;
+    } else {
+      result[m.user_id] = 1;
+    }
+  });
+
+  return result;
+}
+
+/**
+ * Returns dictionary mapping user_id to number of likes receieved
+ */
+function countLikes() {
+  members = conversation.members;
+  result = {}
+  messages.forEach(m => {
+    if (result.hasOwnProperty(m.user_id)) {
+      result[m.user_id] += m.favorited_by.length;
+    } else {
+      result[m.user_id] = m.favorited_by.length;
+    }
+  })
+  return result;
 }
