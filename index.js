01b8653237bb5ace95eff985c48491fd5c7a77b3
Andrew Jivoin <andrew@jivoin.com>
2018-11-18 14:22:51 -0500

    Functions

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ document.getElementById('importMessage').onclick = function() {
  * Returns a dictionary mapping user_id to a list of their messages
  */
 function mapUserToMessages() {
-  members = conversation.members;
-  result = {}
+  let result = {};
   
   messages.forEach(m => {
     if (result.hasOwnProperty(m.user_id)) {
@@ -63,8 +62,7 @@ function mapUserToMessages() {
  * Returns dictionary mapping user_id to number of messages sent
  */
 function countMessages() {
-  members = conversation.members;
-  result = {}
+  let result = {};
   
   messages.forEach(m => {
     if (result.hasOwnProperty(m.user_id)) {
@@ -80,15 +78,50 @@ function countMessages() {
 /**
  * Returns dictionary mapping user_id to number of likes receieved
  */
-function countLikes() {
-  members = conversation.members;
-  result = {}
+function countLikesReceived() {
+  let result = {};
   messages.forEach(m => {
     if (result.hasOwnProperty(m.user_id)) {
       result[m.user_id] += m.favorited_by.length;
     } else {
       result[m.user_id] = m.favorited_by.length;
     }
-  })
+  });
   return result;
+}
+
+function countLikesGiven() {
+  members = conversation.members;
+  let result = {};
+  messages.forEach(m => {
+    m.favorited_by.forEach(uid => {
+      if (result.hasOwnProperty(uid)) {
+        result[uid]++;
+      } else {
+        result[uid] = 1;
+      }
+    });
+  });
+  return result;
+}
+
+/**
+ * Returns average likes per message for each active member
+ */
+function likesPerMessage() {
+  let result = {};
+  const received = countLikesReceived();
+  const messages = countMessages();
+  
+  for (const member of conversation.members) {
+    let uid = member.user_id;
+    result[uid] = received[uid] / messages[uid];
+  }
+
+  return result;
+}
+
+function mostLikedPerson() {
+  const received = countLikesReceived();
+  return Object.keys(received).reduce((a, b) => received[a] > received[b] ? a : b);
 }
